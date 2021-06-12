26013539f4ec42327f225502036c0966a560085d
Tatiane Chiarello <tatichiarello@gmail.com>
2021-06-12 13:20:22 +0200

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: "Wine Club",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: ["gatsby-plugin-gatsby-cloud", `gatsby-plugin-styled-components`],
 };
--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import * as React from "react";
 
-export default function IndexPage() {
+export default function Header() {
   return <div>OI</div>;
 }
