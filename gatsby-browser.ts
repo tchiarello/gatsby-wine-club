05b0a2cbb356bcbf5b6de6d6bc03c74c9ba11231
Tatiane Chiarello <tatichiarello@gmail.com>
2023-08-10 15:26:38 +0200

    fix: route issue

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,1 +1,8 @@
-import "./src/styles/global.css";
+import './src/styles/global.css';
+
+export const onInitialClientRender = () => {
+  window.addEventListener(
+    'popstate',
+    () => (window.location.href = window.location.href),
+  );
+};
